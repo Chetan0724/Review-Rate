599b7d50396ebe60e9fd660bdd4096c23f614cfa
Chetan Yadav <ydvchetan01@gmail.com>
2025-11-25 18:38:08 +0530

    review and user models

---
+++ backend/src/models/review.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema(
+  {
+    companyId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Company",
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    reviewText: { type: String, required: true },
+    rating: { type: Number, required: true, min: 1, max: 5 },
+  },
+  { timestamps: true }
+);
+
+export const Review = mongoose.model("Review", reviewSchema);
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    avatar: { type: String },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
